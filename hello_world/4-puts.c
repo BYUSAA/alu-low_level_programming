d53861fc0d87f4f2c98c812422de91a3613309ca
BYUUSAA <byuussaa@gmail.com>
2026-09-01 15:03:27 +0100

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *  main - starting point for program execution
+ * description - main will be used as function
+ *  Return: 0 (success)
  */
+
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
