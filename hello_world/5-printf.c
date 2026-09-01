782eb1de23845e0650a1e02247d404e0fe848cc4
BYUUSAA <byuussaa@gmail.com>
2026-09-01 15:03:48 +0100

    Update 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main - starting point for program execution
+ *printf - send formatted output to the screen
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
