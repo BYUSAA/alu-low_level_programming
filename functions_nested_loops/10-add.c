6f5f7b18a48dd276eec250928dce7e49d0456b62
BYUUSAA <byuussaa@gmail.com>
2026-09-14 11:15:18 +0100

    Update 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,39 +1,13 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: Nothing.
+ * Return: Sum of a and b.
  */
-void times_table(void)
+int add(int a, int b)
 {
-	int row;
-	int column;
-	int result;
-
-	for (row = 0; row <= 9; row++)
-	{
-		for (column = 0; column <= 9; column++)
-		{
-			result = row * column;
-
-			if (column == 0)
-				_putchar(result + '0');
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (result < 10)
-					_putchar(' ');
-
-				if (result >= 10)
-					_putchar((result / 10) + '0');
-
-				_putchar((result % 10) + '0');
-			}
-		}
-
-		_putchar('\n');
-	}
+	return (a + b);
 }
